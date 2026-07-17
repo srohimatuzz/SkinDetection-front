8192acc15b102809d756bf46177c1a3f461cbecd
Siti Rohimatuz Zahro <sukarami.1996@gmail.com>
2026-07-18 00:39:49 +0700

    perbaaikan server

--- src/views/ScanView.vue
+++ src/views/ScanView.vue
@@ -321,6 +321,8 @@ async function analyzeImage() {
 
     const API_URL =
       import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000'
+      
+      console.log("API_URL =", API_URL)
 
     const response = await axios.post(
       `${API_URL}/analyze`,
